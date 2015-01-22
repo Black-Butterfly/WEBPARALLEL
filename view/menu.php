92a2aca1769d50e0d9349f1b0318ef0b9fb7e368
Kan23 <romain.lelievre93@gmail.com>
2015-01-22 12:29:32 +0100

    fiojsjgoxdfohdoghdgdowh

--- view/menu.php
+++ view/menu.php
@@ -1,3 +1,8 @@
+<?php
+if(!isset($_REQUEST['uc'])){
+     	$_REQUEST['uc'] = 'home';
+	}
+?>
 <nav class="navbar navbar-inverse">
 			  <div class="container-fluid">
 			  	<!-- Voir pourquoi le bouton ne marche pas !! -->
@@ -11,16 +16,43 @@
 			    </div>
 			    <div class="navbar-collapse collapse" id="myNavbar">
 			      <ul class="nav navbar-nav">
-			        <li class="active"><a href="index.php?uc=home">Home</a></li>
-			        <li><a href="index.php?uc=meetings">Réunions</a></li>
-			        <?php if(isset($_SESSION['NOM'])){ ?>
-			        <li><a href="index.php?uc=create">Création Réunion</a></li>
+					<?php 
+						if($_REQUEST['uc'] == 'home')
+							echo "<li class='active'>";
+						else
+							echo "<li>";
+					?>
+					<a href="index.php?uc=home">Home</a></li>
+			        <?php 
+						if($_REQUEST['uc'] == 'meetings')
+							echo "<li class='active'>";
+						else
+							echo "<li>";
+					?>
+					<a href="index.php?uc=meetings">Réunions</a></li>
+			        <?php if(isset($_SESSION['NOM'])){
+							if($_REQUEST['uc'] == 'create')
+								echo "<li class='active'>";
+							else
+								echo '<li>';
+					?>
+					<a href="index.php?uc=create">Création Réunion</a></li>
 			        <?php } ?>
 			      </ul>
 			      <ul class="nav navbar-nav navbar-right">
-			      	<?php if(!isset($_SESSION['NOM'])){ ?>
-						  <li><a href="index.php?uc=register"><span class="glyphicon glyphicon-user"> </span> Register</a></li>
-			        	  <li><a href="index.php?uc=login"><span class="glyphicon glyphicon-log-in"> </span> Login</a></li>
+			      	<?php if(!isset($_SESSION['NOM'])){ 
+							if($_REQUEST['uc'] == 'register')
+								echo "<li class='active'>";
+							else
+								echo "<li>";
+						?>
+						  <a href="index.php?uc=register"><span class="glyphicon glyphicon-user"> </span> Register</a></li>
+			        	  <?php if($_REQUEST['uc'] == 'login')
+									echo "<li class='active'>";
+								else
+									echo "<li>";
+						  ?>
+						  <a href="index.php?uc=login"><span class="glyphicon glyphicon-log-in"> </span> Login</a></li>
 					  <?php }else{ ?>
 						  <li><a href="index.php?uc=user"><span class="glyphicon glyphicon-user"> </span> <?php echo $_SESSION['PRENOM'];?> </a></li>
 			        	  <li><a href="index.php?uc=logout"><span class="glyphicon glyphicon-log-in"> </span> Logout</a></li>
