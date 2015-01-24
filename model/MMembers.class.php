cc15b95db3e34dc1f2e79183c57c2fd379aad315
Black-Butterfly <ludovic.romano@live.fr>
2015-01-24 22:14:28 +0100

    MMembers commenté

--- model/MMembers.class.php
+++ model/MMembers.class.php
@@ -3,6 +3,8 @@
 *	@file   MMembers.class.php
 * 
 *	@author Black Butterfly 
+*
+*	@update Andujar Mariana
 * 
 *	@date   21/01/2015
 * 
@@ -19,7 +21,24 @@ class MMembers{
 
     public function __destruct () {}
 	
-	//Ajout d'un utilisateur
+	/*
+	*
+	*	@function 		Add_Member
+	*
+	*	@description	Cette fonction permet de rajouter un utilisateur dans la 
+	*					base
+	*
+	*	@Param 			$log	 		( Login de l'utilisateur )
+	*	@Param 			$name			( Nom de l'utilisateur )
+	*	@Param 			$surname 		( Prénom de l'utilisateur )
+	*	@Param 			$mail			( Adresse mail de l'utilisateur )
+	*	@Param 			$passwd			( Mot de passe buildé )
+	*	@Param 			$tel			( Numéro de telephone )
+	*	@Param			$salt			( Le salt générer en random )
+	*
+	*	@return			Rien
+	*
+	*/
     public function Add_Member ($log, $name, $surname, $mail, $passwd, $tel, 
 		$salt)
     {
@@ -36,7 +55,7 @@ class MMembers{
 			
 			// Etant donnée qu'une sqli est passé avec l'utilisation seule de 
 			// la méthode prepare de PDO, en utilisant la methode bind en plus
-			// cela protège apparement mieux
+			// cela protège apparement mieux, mais pas encore assé ...
 			$reqprep->bindParam(1, $log, 		PDO::PARAM_STR);
 			$reqprep->bindParam(2, $name, 		PDO::PARAM_STR);
 			$reqprep->bindParam(3, $surname, 	PDO::PARAM_STR);
@@ -51,8 +70,22 @@ class MMembers{
 		}catch (PDOException $e){
 			die("exception : ". $e->getMessage());
 		}	
-    }
+    }// Add_Member
 	
+	
+	/*
+	*
+	*	@function 		Get_Info
+	*
+	*	@description	Cette fonction permet de récupérer le mdp et le salt 
+	*					hashé
+	*
+	*	@Param 			$login 		( Le login de l'utilisateur )
+	*
+	*	@return			row : 0 mdp
+	*					1 salt
+	*
+	*/
 	public function Get_Info($login)
 	{
 		try{
@@ -74,8 +107,21 @@ class MMembers{
 		}
 		return $result;
 		
-	}
+	}// Get_Info
 	
+	
+	/*
+	*
+	*	@function 		Who_I_Am
+	*
+	*	@description	Permet de savoir si un compte existe déjà
+	*
+	*	@Param 			$login 		( Login de l'utilisateur )
+	*
+	*	@return			True si existe
+	*					False sinon
+	*
+	*/
 	public function Who_I_Am($login)
 	{
 		try{
@@ -95,8 +141,25 @@ class MMembers{
 			die("exception");
 		}	
 		return ($reqprep->rowCount() == '1') ? TRUE : FALSE;
-	}
+	}// Who_I_Am
 	
+	
+	/*
+	*
+	*	@function 		update_User
+	*
+	*	@description	Cette fonction permet de modifier les informations 
+	*					générale d'un utilisateur
+	*
+	*	@Param 			$id 		( Sujet du meeting )
+	*	@Param 			$name		( description du meeting )
+	*	@Param 			$surname 	( lieux du meeting )
+	*	@Param 			$tel		( Temps du meeting (h) )
+	*	@Param 			$email		( Temps du meeting (mn) )
+	*
+	*	@return			Rien
+	*
+	*/
 	public function update_User($id, $name, $surname, $tel, $email){
 		try{
 			// connexion
@@ -107,6 +170,7 @@ class MMembers{
 					TEL = ?, EMAIL = ? 
 					WHERE ID_USER = ?;";
 			$reqprep = $cnx->prepare($req);
+			// Protéction sqli
 			$reqprep->bindParam(1, $name, 		PDO::PARAM_STR);
 			$reqprep->bindParam(2, $surname, 	PDO::PARAM_STR);
 			$reqprep->bindParam(3, $tel, 		PDO::PARAM_STR);
@@ -120,8 +184,25 @@ class MMembers{
 			die("exception : ". $e->getMessage());
 		}
 		
-	}
+	}// update_User
 	
+	
+	/*
+	*
+	*	@function 		getUser
+	*
+	*	@description	Récupère les info de base d'un utilisateur
+	*
+	*	@Param 			$login 		( login de l'utilisateur)
+	*
+	*	@return			Row : 
+	*	@return			Id_User
+	*	@return			Name
+	*	@return			Surname
+	*	@return			Tel
+	*	@return			Email
+	*
+	*/
 	public function getUser($login)
 	{
 		try{
@@ -143,8 +224,22 @@ class MMembers{
 			die("exception");
 		}	
 		return $result;
-	}
+	} // getUser
 	
+	
+	/*
+	*
+	*	@function 		updateMdp
+	*
+	*	@description	Cette fonction permet de modifier le mdp d'un 
+	*					utilisateur
+	*
+	*	@Param 			$id 		( Id de l'utilisateur )
+	*	@Param 			$mdp		( nouveau mot de passe )
+	*
+	*	@return			Rien
+	*
+	*/
 	public function updateMdp($id,$mdp){
 		try{
 				// connexion
@@ -164,6 +259,19 @@ class MMembers{
 		}
 	}
 	
+	
+	/*
+	*
+	*	@function 		getLoginById
+	*
+	*	@description	Cette fonction permet de récupérer le login ainsi que
+	*					le salt
+	*
+	*	@Param 			$id 		( id de l'utilisateur )
+	*
+	*	@return			row : 0 --> Login  ; 1 --> Salt 
+	*
+	*/
 	public function getLoginById($id){
 		try{
 			// connexion
@@ -173,6 +281,7 @@ class MMembers{
 			// preparer la requete
 			$req = "SELECT LOGIN, SALT FROM USER WHERE ID_USER = ?;";
 			$reqprep = $cnx->prepare($req);
+			// Protéction sqli
 			$reqprep->bindParam(1, $id, 	PDO::PARAM_STR);
 			$reqprep->execute(array($id));
 			$result = $reqprep->fetch();
@@ -183,5 +292,5 @@ class MMembers{
 			die("exception");
 		}	
 		return $result;
-	}
+	} // getLoginById
 }
